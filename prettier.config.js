b790a67e3fbaa8c4678c3bb0ba7537e7da63759e
Ahmed Bayoumi <bayoumi.dev@gmail.com>
2022-06-27 09:58:58 +0200

    prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -1,10 +1,11 @@
 module.exports = {
    trailingComma: 'es5',
    tabWidth: 3,
-   printWidth: 100,
+   printWidth: 80,
    semi: false,
    singleQuote: true,
    bracketSpacing: true,
+   bracketSameLine: true,
    arrowParens: 'avoid',
    proseWrap: 'always',
-}
+};
